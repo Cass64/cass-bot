8ff1c4191b0c95738ddfbeb3ee2a8410cdda2bfd
Cass64 <nolhan.vandenbroeck@gmail.com>
2025-01-12 01:02:57 +0100

    Update start.py

--- start.py
+++ start.py
@@ -8,37 +8,21 @@ load_dotenv()
 token = os.getenv('TOKEN_BOT_DISCORD')
 
 class MonBot(commands.Bot):
-  async def setup_hook(self):
-          cogs = ['cogs.games', 'cogs.moderation', 'cogs.events'] 
-          for cog in cogs:
-        # Vérifie si le cog est déjà chargé
-           if cog not in bot.extensions:
-            try:
-                bot.load_extension(cog)
-                print(f"Extension {cog} chargée avec succès.")
-            except Exception as e:
-                print(f"Échec du chargement de {cog}: {str(e)}")
-           else:
-            print(f"Extension {cog} déjà chargée, rien à faire.")
-
+    async def setup_hook(self):
+        cogs = ['cogs.games', 'cogs.moderation', 'cogs.events']
+        for cog in cogs:
+            if cog not in self.extensions:  # Vérifie si le cog est déjà chargé
+                try:
+                    await self.load_extension(cog)
+                    print(f"Extension {cog} chargée avec succès.")
+                except Exception as e:
+                    print(f"Échec du chargement de {cog}: {str(e)}")
+            else:
+                print(f"Extension {cog} déjà chargée.")
 
 intents = discord.Intents.all()
 bot = MonBot(command_prefix='!', intents=intents)
 
 keep_alive()
-
-print(f"Token récupéré : {token}")  # Afficher le token
-
-bot.run(token)
-
-      
-
-
-intents = discord.Intents.all()
-bot = MonBot(command_prefix='!', intents=intents)
-
-keep_alive()
-
-print(f"Token récupéré : {token}")  # Afficher le token
 
 bot.run(token)
