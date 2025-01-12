ff0de94a2e7c11f9e864ac3a4c9ba01dac775edc
Cass64 <nolhan.vandenbroeck@gmail.com>
2025-01-12 01:22:31 +0100

    Update start.py

--- start.py
+++ start.py
@@ -2,18 +2,33 @@ import os
 from dotenv import load_dotenv
 import discord
 from discord.ext import commands
+import random
 from keep_alive import keep_alive
 
 load_dotenv()
 token = os.getenv('TOKEN_BOT_DISCORD')
 
-class MonBot(commands.Bot):
-    async def setup_hook(self):
-   
-
+# Crée un objet bot
 intents = discord.Intents.all()
-bot = MonBot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
 
-# Démarre le bot une seule fois ici
+# Commande pour le jeu "roll"
+@bot.command()
+async def roll(ctx):
+    result = random.randint(1, 6)
+    await ctx.send(f'{ctx.author.name} lance un dé et obtient : {result}')
+
+# Commande pour le jeu "pile ou face"
+@bot.hybrid_command()
+async def pileouface(ctx):
+    result = random.choice(['pile', 'face'])
+    await ctx.send(f'Résultat obtenu : {result}')
+
+# Quand le bot est prêt
+@bot.event
+async def on_ready():
+    print(f"Le bot est prêt et connecté en tant que {bot.user}")
+
+# Démarrer le bot
 keep_alive()
 bot.run(token)
