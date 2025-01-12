fc27170eca20add32625e7726ac4b58a1f9ca6b5
Cass64 <nolhan.vandenbroeck@gmail.com>
2025-01-12 01:19:30 +0100

    Update start.py

--- start.py
+++ start.py
@@ -9,19 +9,7 @@ token = os.getenv('TOKEN_BOT_DISCORD')
 
 class MonBot(commands.Bot):
     async def setup_hook(self):
-        # Liste des cogs à charger
-        cogs = ['cogs.games', 'cogs.moderation', 'cogs.events']
-        
-        for cog in cogs:
-            # On vérifie si le cog est déjà chargé avant de le charger à nouveau
-            if cog not in self.extensions:
-                try:
-                    await self.load_extension(cog)
-                    print(f"Extension {cog} chargée avec succès.")
-                except Exception as e:
-                    print(f"Échec du chargement de {cog}: {str(e)}")
-            else:
-                print(f"Extension {cog} déjà chargée, rien à faire.")
+   
 
 intents = discord.Intents.all()
 bot = MonBot(command_prefix='!', intents=intents)
